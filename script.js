44d4b8e56dab93c713034fe3632f629d83ccab47
kepplin <pranavborse91@gmail.com>
2023-08-12 22:00:40 +1000

    Finish matches played code

--- script.js
+++ script.js
@@ -24,3 +24,28 @@ function getOccurence(array, value) {
   array.forEach((x) => x === value && count++);
   return count;
 }
+
+// optimise this in the future
+let matchesPlayedPranav = document.querySelector(".matchesPlayedPranav");
+let matchesPlayedAryan = document.querySelector(".matchesPlayedAryan");
+let matchesPlayedKedar = document.querySelector(".matchesPlayedKedar");
+let matchesPlayedSoham = document.querySelector(".matchesPlayedSoham");
+let matchesPlayedRoshan = document.querySelector(".matchesPlayedRoshan");
+let matchesPlayedKunal = document.querySelector(".matchesPlayedKunal");
+let matchesPlayedSahil = document.querySelector(".matchesPlayedSahil");
+
+let matchesPranav = getOccurence(finalArray, "Pranav");
+let matchesAryan = getOccurence(finalArray, "Aryan");
+let matchesKedar = getOccurence(finalArray, "Kedar");
+let matchesSoham = getOccurence(finalArray, "Soham");
+let matchesRoshan = getOccurence(finalArray, "Roshan");
+let matchesKunal = getOccurence(finalArray, "Kunal");
+let matchesSahil = getOccurence(finalArray, "Sahil");
+
+matchesPlayedPranav.textContent = matchesPranav;
+matchesPlayedAryan.textContent = matchesAryan;
+matchesPlayedKedar.textContent = matchesKedar;
+matchesPlayedSoham.textContent = matchesSoham;
+matchesPlayedRoshan.textContent = matchesRoshan;
+matchesPlayedKunal.textContent = matchesKunal;
+matchesPlayedSahil.textContent = matchesSahil;
