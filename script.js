f7f4be40d0621b055379bde2c6e9ade724e96a95
kepplin <pranavborse91@gmail.com>
2023-08-12 21:27:49 +1000

    Create basic function for matches played
    It basically works like this:
    1. Have one main array at global scope
    2. Run a ".forEach" on the 'teamOne' array.
    3. Inside of the forEach, get the text content for each of the array values.
    4. Now that you have the textcontent, convert it into an array using .split(',')
    which splits at the comma. This essentially seperates the players into individual
    elements.
    5. We then add our new seperate arrays into our main array using ".push"
    6. We then convert our main array into a string using ".toString()"
    7. We then convert this string back into an array using ".split()", to get
    each name seperated
    
    So we started with 'Pranav, Aryan, Kedar', 'Kunal, Soham, Sahil, Roshan'
    which converted to ['Pranav', 'Aryan', 'Kedar'], ['Kunal', 'Soham', 'Sahil', 'Roshan']
    then [['Pranav', 'Aryan', 'Kedar'], ['Kunal', 'Soham', 'Sahil', 'Roshan']]
    (both seperate arrays inside of the main array), then we convert it all into one string:
    'Pranav,Aryan,Kedar,Kunal,Soham,Roshan', and finally its converted all into
    an array again:
    ['Pranav', 'Aryan', 'Kedar', 'Kunal', ' Soham', ' Roshan', ' Sahil']

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Teams
-let teamOne = document.querySelectorAll(".teamOne");
+let teamOne = Array.from(document.querySelectorAll(".teamOne"));
+
 let teamTwo = document.querySelectorAll(".teamTwo");
 
 // Scores
 let teamOneScore = document.querySelectorAll(".teamOneScore");
 let teamTwoScore = document.querySelectorAll(".teamTwoScore");
+// Matches Played
+let bigArray = [];
+teamOne.forEach((element) => {
+  abc = element.textContent;
+  let abcsplit = abc.split(",");
+  abcsplit.tostr;
+  console.log(abcsplit);
+  bigArray.push(abcsplit);
+});
+stringArray = bigArray.toString();
+let array = stringArray.split(",");
+console.log(array);
+if (array.includes("Kunal")) {
+  console.log("hi");
+}
