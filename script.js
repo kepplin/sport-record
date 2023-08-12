9b5adc73eb71648d73c7f1f108cfc0aff1088775
kepplin <pranavborse91@gmail.com>
2023-08-12 20:47:49 +1000

    Add placeholder data to table

---
+++ script.js
@@ -0,0 +1,7 @@
+// Teams
+let teamOne = document.querySelectorAll(".teamOne");
+let teamTwo = document.querySelectorAll(".teamTwo");
+
+// Scores
+let teamOneScore = document.querySelectorAll(".teamOneScore");
+let teamTwoScore = document.querySelectorAll(".teamTwoScore");
